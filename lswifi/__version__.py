a4f02fd1f6ce61069b79e1e27a94b3d1992dee3f
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2021-04-18 17:46:05 -0400

    bump version because push to PyPI issue

--- lswifi/__version__.py
+++ lswifi/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "lswifi"
 __description__ = "a CLI Wi-Fi scanner tool for Windows"
 __url__ = "https://github.com/joshschmelzle/lswifi"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __author__ = "Josh Schmelzle"
 __author_email__ = "josh@joshschmelzle.com"
 __license__ = "BSD-3-Clause"
