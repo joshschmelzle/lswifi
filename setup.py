9335eab5c586b0d34703b3fb8ea0cae0fcd37598
Josh Schmelzle <josh@joshschmelzle.com>
2022-02-13 21:19:19 -0500

    add Py3.10 classifier to setup()

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Information Technology",
         "Intended Audience :: System Administrators",
         "Topic :: Utilities",
