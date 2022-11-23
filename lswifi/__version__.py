ac5e43995ed9878903948fab24ee0f13d2a21310
Josh Schmelzle <josh@joshschmelzle.com>
2022-11-22 20:47:52 -0500

    match github description

--- lswifi/__version__.py
+++ lswifi/__version__.py
@@ -9,7 +9,7 @@
 """ version information for lswifi """
 
 __title__ = "lswifi"
-__description__ = "a CLI Wi-Fi scanner tool for Windows"
+__description__ = "a CLI-centric Wi-Fi scanning tool for Windows"
 __url__ = "https://github.com/joshschmelzle/lswifi"
 __version__ = "0.1.32"
 __author__ = "Josh Schmelzle"
